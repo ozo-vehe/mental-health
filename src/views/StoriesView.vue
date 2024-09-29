f98398991d78f7c2150fd0ffe057898a78813391
ozo-vehe <johnnie.vehe@gmail.com>
2024-09-29 12:05:49 +0100

    edited the stories page

--- src/views/StoriesView.vue
+++ src/views/StoriesView.vue
@@ -4,12 +4,12 @@ import ButtonComponent from "../components/ButtonComponent.vue";
 
 <template>
   <header
-    class="home_hero overflow-hidden relative min-h-[100vh] flex items-center justify-center lg:px-20 md:px-12 px-4">
+    class="home_hero overflow-hidden relative lg:min-h-[100vh] md:min-h-[90vh] min-h-[80vh] flex items-end justify-center lg:px-20 md:px-12 px-4 pb-20">
     <img class="absolute top-0 left-0 w-full h-full object-cover" src="../assets/images/stories_header_image.png"
       alt="Hero Image" />
 
     <div class="hero_text text-center relative text-mh_light max-w-[800px]">
-      <h1 class="text-mh_large font-[700] leading-tight">
+      <h1 class="lg:text-mh_large md:text-mh_h1 text-mh_h1 font-[700] leading-tight">
         Your story can give hope to someone today!
       </h1>
 
@@ -24,15 +24,16 @@ import ButtonComponent from "../components/ButtonComponent.vue";
     <!-- Men's Challenge -->
     <section
       class="bg-[#efefef] lg:px-20 md:px-12 px-4 py-20 flex flex-wrap items-center justify-center gap-x-16 gap-y-4">
-      <div class="image_container max-w-[500px] max-h-[500px] relative p-4">
+      <div class="image_container max-w-[500px] max-h-[500px] relative p-4 lg:order-1 md:order-1 order-2">
         <img class="w-full h-full object-fit" src="../assets/images/about_image.png" alt="Experience Image" />
       </div>
 
-      <div class="max-w-[650px]">
-        <h3 class="px-3 font-[600] text-mh_h2 relative mb-2">
+      <div class="max-w-[650px] lg:order-2 md:order-2 order-1">
+        <h3 class="px-3 font-[700] lg:text-mh_h2 md:text-mh_h3 text-mh_h4 relative mb-2 leading-tight lg:text-left md:text-left text-center">
           Men go through challenges too
         </h3>
-        <p class="font[500] text-[22px] px-3 font-[500]">
+        <p
+          class="font[500] lg:text-[22px] md:text-mh_h6 text-[18px] px-3 font-[500] lg:text-left md:text-left text-center">
           Men's health impacts far more than just men themselves. It starts with
           individuals but exends to families, workplaces, communities, and even influences
           health systems and economies. <br /><br />
@@ -44,11 +45,22 @@ import ButtonComponent from "../components/ButtonComponent.vue";
           These personal stories are crucial in illustrating the real-world impact of
           men's health issues.
         </p>
+
+        <div class="lg:hidden md:hidden w-full flex flex-col items-center justify-center gap-12 mt-8">
+          <p class="text-[18px] font-[500] text-center">
+            By sharing, you help amplify the call for healthcare systems that address the
+            needs of all genders and advocate for stronger government action on men's health
+            issues. It only takes a couple of minutes to make a difference.
+          </p>
+
+          <ButtonComponent text="Share your experience"
+            mh_class="bg-[#130D06] font-bold px-12 text-mh_light text-[18px] py-3 lg:w-[50%] md:w-[70%] w-full" />
+        </div>
       </div>
     </section>
 
     <section
-      class="bg-[#efefef] flex flex-wrap items-center justify-center gap-x-16 gap-y-4 lg:px-20 md:px-12 px-4 pb-20">
+      class="bg-[#efefef] lg:flex md:flex hidden flex-wrap items-center justify-center gap-x-16 gap-y-4 lg:px-20 md:px-12 px-4 pb-20">
       <div class="lg:w-[70%] md:w-[90%] w-full flex flex-col items-center justify-center gap-8">
         <p class="text-mh_h6 font-[500]">
           By sharing, you help amplify the call for healthcare systems that address the
@@ -63,4 +75,5 @@ import ButtonComponent from "../components/ButtonComponent.vue";
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
